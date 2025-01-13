41196ea95396396a78440047a72f5cf223487b30
Augusto Maldonado <augusto.maldonado@openk.com.br>
2025-01-13 10:30:56 -0300

    feat: adicionando list-item

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,3 +1,32 @@
-export function ListItem() {
-  return <h1 className="flex flex-col bg-black text-center"></h1>;
+import { CircleCheck, Trash2 } from "lucide-react";
+import { useState } from "react";
+
+interface ListItemProps {
+  description: string;
+}
+
+export function ListItem({ description }: ListItemProps) {
+  const [taskCheck, setTaskCheck] = useState(false);
+
+  return (
+    <div className="mb-3 flex h-20 w-full items-start justify-around gap-4 rounded-lg bg-[#262626] px-6 py-4">
+      {!taskCheck ? (
+        <button
+          className="mt-1 h-6 w-6 rounded-full border-2 border-[#4EA8DE]"
+          onClick={() => setTaskCheck(true)}
+        ></button>
+      ) : (
+        <CircleCheck
+          color="#4EA8DE"
+          className="mt-1 h-6 w-6"
+          onClick={() => setTaskCheck(false)}
+        />
+      )}
+
+      <p className="text-[#F2F2F2]">{description}</p>
+      <button className="mt-1">
+        <Trash2 color="#808080" size={24} />
+      </button>
+    </div>
+  );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,14 @@
 import { BookmarkX } from "lucide-react";
+import { useState } from "react";
+import { ListItem } from "./ListItem";
+
+export type ListItem = {
+  description: string;
+};
 
 export function TodoList() {
+  const [listItems, setListItems] = useState<ListItem[]>();
+
   return (
     <div className="flex h-3/4 w-full flex-col items-center justify-center">
       <div className="flex w-full justify-between px-8 pb-6">
@@ -17,14 +25,24 @@ export function TodoList() {
           </span>
         </p>
       </div>
-      <div className="flex h-60 w-full flex-col items-center justify-center rounded-lg border-t-[1px] border-[#333333] text-center">
-        <BookmarkX color="#808080" size={56} className="mb-4" />
-        <p className="font-sans font-bold text-[#808080]">
-          Você ainda não tem tarefas cadastradas
-        </p>
-        <p className="font-sans text-[#808080]">
-          Crie tarefas e organize seus itens a fazer
-        </p>
+      <div
+        className={`flex h-60 w-full flex-col items-center rounded-lg border-t-[1px] border-[#333333] ${!listItems && "justify-center"}`}
+      >
+        {listItems && listItems?.length > 0 ? (
+          listItems.map((item, i) => (
+            <ListItem key={i} description={item.description} />
+          ))
+        ) : (
+          <>
+            <BookmarkX color="#808080" size={56} className="mb-4" />
+            <p className="font-sans font-bold text-[#808080]">
+              Você ainda não tem tarefas cadastradas
+            </p>
+            <p className="font-sans text-[#808080]">
+              Crie tarefas e organize seus itens a fazer
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
